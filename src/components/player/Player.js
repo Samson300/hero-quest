ecee4055ce9c9351cfd30143584fa0023fa2d608
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-22 12:01:54 -0400

    changes player to a class with state, hp, attack, level, exp, alive, gold, inventory

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -4,20 +4,34 @@ import walkSprite from '../../styles/HeroSprites/Characters/player_walk1.png'
 import PlayerMovement from './PlayerMovement';
 
 //This controlls how the player is styled on the screen
-function Player(props) {
-    return (
-        <div
-        style={{
-            position: 'absolute',
-            top: props.position[1],
-            left: props.position[0],
-            backgroundImage: `url('${walkSprite}')`,
-            backgroundPosition: props.spriteLocation, // This is what displays the characters; currently, displaying incorrectly initally
-            width: '32px',
-            height: '32px'
-        }}
-        />
-    )
+class Player extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state={
+            hp: 100,
+            attack: 20,
+            level: 1,
+            exp: 0,
+            alive: true,
+            gold: 0,
+            inventory: []
+        }
+    }
+    render() {
+        return (
+            <div
+            style={{
+                position: 'absolute',
+                top: this.props.position[1],
+                left: this.props.position[0],
+                backgroundImage: `url('${walkSprite}')`,
+                backgroundPosition: this.props.spriteLocation, // This is what displays the characters; currently, displaying incorrectly initally
+                width: '32px',
+                height: '32px'
+            }}
+            />
+        )
+    }
 }
 
 // maps the state to props(allows us to access the state via props)
