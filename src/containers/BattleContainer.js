1cdcb6dc65e7a47d57db70d2d3a124466a69e766
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-23 17:30:30 -0400

    adds monster attack and player attack in single button

--- src/containers/BattleContainer.js
+++ src/containers/BattleContainer.js
@@ -33,7 +33,6 @@ const mapDispatchToProps = (dispatch) => {
             dispatch({ type: 'BATTLE_END', payload: {
                 exp: 100,
                 gold: 10,
-                monsterHP: 100
             }})
         }
     }
