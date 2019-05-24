3da6ef793240606bdbcf399f253a8852d996dd6b
Ryan Yim <yknyim@gmail.com>
2019-05-23 20:29:55 -0400

    increments player's attack and hp after leveling up

--- src/reducers/monsterReducer.js
+++ src/reducers/monsterReducer.js
@@ -1,6 +1,14 @@
 const initialState = {
     monsterHP: 100,
-    monsterAttack: 10,
+
+    // monsterAttack is moved to playerReducer.js in order to damage player.
+    // monsterAttack: 10,
+
+    // playerAttack is needed to damage the monster and have an incrementing value
+    // as the player levels up.
+    playerAttack: 20,
+    // See case 'PLAYER_ATTACK' & 'LEVEL_UP'
+    
     monsterLevel: 1
 }
 
@@ -9,13 +17,22 @@ const monsterReducer = (state=initialState, action) => {
         case 'PLAYER_ATTACK':
             return {
                 ...state,
-                monsterHP: state.monsterHP - action.payload.dmg
+                // Instead of action.payload.dmg, state.playerAttack from the monsterReducer's
+                // initialState will provide the value to damage the monster.
+                monsterHP: state.monsterHP - state.playerAttack
             };
         case 'BATTLE_END':
                 return {
                     ...state,
                     monsterHP: 100
                 }
+
+        // The player's attack will increase as the player levels up.
+        case 'LEVEL_UP':
+        return {
+            ...state,
+            playerAttack: state.playerAttack + action.payload.playerAtk
+        }
         default:
             return state;
     }
