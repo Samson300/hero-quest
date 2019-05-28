d328e4d779b8103bc3669a30b6e6230f6de0ba1e
Anthony Moss <asmoss93@gmail.com>
2019-05-27 22:07:42 -0400

    adds dungeon boss component basics

--- src/reducers/dungeonBossReducer.js
+++ src/reducers/dungeonBossReducer.js
@@ -1,13 +1,17 @@
 const initialState = {
-    caveBossHP: 500,
-    caveBossHPInitial: 500,
-    caveBossAttack: 50,
+    caveBossHP: 350,
+    caveBossAttack: 35,
+    // monsterAttack is moved to playerReducer.js in order to damage player.
+    // monsterAttack: 10,
+
     // playerAttack is needed to damage the monster and have an incrementing value
     // as the player levels up.
     playerAttack: 10,
+    // See case 'PLAYER_ATTACK' & 'LEVEL_UP'
+    
     monsterLevel: 1,
     bossDisplay: 'none',
-    backgroundPosition: 'left top',
+    // backgroundPosition: 'left top',
     top: 20,
     left: 260
 }
@@ -24,7 +28,7 @@ const caveBossReducer = (state=initialState, action) => {
         case 'BATTLE_END':
             return {
                 ...state,
-                caveBossHP: state.caveBossHPInitial
+                caveBossHP: 350
             }
         // The player's attack will increase as the player levels up.
         case 'LEVEL_UP':
@@ -38,12 +42,12 @@ const caveBossReducer = (state=initialState, action) => {
                 ...state,
                 playerAttack: state.playerAttack + action.payload.playerAttack
             }
-        case 'DISPLAY_CAVE_BOSS':
+        case 'DISPLAY_DUNGEON_BOSS':
             return {
                 ...state,
                 ...action.payload
             }
-        case 'CAVE_BOSS_BATTLE':
+        case 'DUNGEON_BOSS_BATTLE':
                 return {
                     ...state,
                     ...action.payload,
