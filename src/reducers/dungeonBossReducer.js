695116c6e11ebf96b228ab5cca564497b1662031
Anthony Moss <asmoss93@gmail.com>
2019-05-27 22:29:00 -0400

    updates dungeonBoss with correct reducer names

--- src/reducers/dungeonBossReducer.js
+++ src/reducers/dungeonBossReducer.js
@@ -1,34 +1,26 @@
 const initialState = {
-    caveBossHP: 350,
-    caveBossAttack: 35,
-    // monsterAttack is moved to playerReducer.js in order to damage player.
-    // monsterAttack: 10,
-
-    // playerAttack is needed to damage the monster and have an incrementing value
-    // as the player levels up.
+    dungeonBossHP: 350,
+    dungeonBossAttack: 35,
     playerAttack: 10,
-    // See case 'PLAYER_ATTACK' & 'LEVEL_UP'
-    
     monsterLevel: 1,
     bossDisplay: 'none',
-    // backgroundPosition: 'left top',
     top: 20,
     left: 260
 }
 
-const caveBossReducer = (state=initialState, action) => {
+const dungeonBossReducer = (state=initialState, action) => {
     switch(action.type) {
         case 'PLAYER_ATTACK':
             return {
                 ...state,
                 // Instead of action.payload.dmg, state.playerAttack from the monsterReducer's
                 // initialState will provide the value to damage the monster.
-                caveBossHP: state.caveBossHP - state.playerAttack
+                dungeonBossHP: state.dungeonBossHP - state.playerAttack
             };
         case 'BATTLE_END':
             return {
                 ...state,
-                caveBossHP: 350
+                dungeonBossHP: 350
             }
         // The player's attack will increase as the player levels up.
         case 'LEVEL_UP':
@@ -50,12 +42,11 @@ const caveBossReducer = (state=initialState, action) => {
         case 'DUNGEON_BOSS_BATTLE':
                 return {
                     ...state,
-                    ...action.payload,
-                    backgroundPosition: action.payload.backgroundPosition
+                    ...action.payload
                 }
         default:
             return state;
     }
 }
 
-export default caveBossReducer;
+export default dungeonBossReducer;
