261b8ad68d616e600c8b81751cd5c936a5ce6778
Anthony Moss <asmoss93@gmail.com>
2019-06-15 19:08:14 -0400

    changes the buttons on createUser page to a form with inputs and function to create user

--- src/components/gameMenu/CreateUserPage.js
+++ src/components/gameMenu/CreateUserPage.js
@@ -7,27 +7,33 @@ import './gameMenu.css'
 class CreateUserPage extends React.Component {
     constructor(props) {
         super(props);
-
-        // this.state = {
-        //     character: {},
-        //     messages: [],
-        //     text: ''
-        // }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // async componentDidMount() {
-    //     response = axios.get("http://localhost4000/api");
-    //     this.setState({
-    //         character: response.data
-    //     }, () => {
-    //         console.log('done setting the state');
-    //     });
-    // }
+    async componentDidMount() {
+        response = axios.get("http://localhost4000/api");
+        this.setState({
+            character: response.data
+        }, () => {
+            console.log('done setting the state');
+        });
+    }
 
-    // componentDidMount() {
-    //     this.props.startNewGame()
-    // }
 
+        // async handleSubmit(event) {
+        //     alert('A name was submitted: ' + this.state);
+        //     event.preventDefault();
+        //     const response = await Axios({
+        //         method: 'get',
+        //         url: '/check',
+        //         data: qs.stringify(this.state),
+        //         headers: {
+        //             'Content-Type': 'application/x-www-form-urlencoded'
+        //         }
+        //     });
+        //     console.log(response.data);
+        // }
 
     render() {
         return (
@@ -43,20 +49,48 @@ class CreateUserPage extends React.Component {
                     <h2>Please enter the information below to create an account and play now!</h2>
                     <h2>Already have an account? Click the Login button to Log in with your account information</h2>
                 </div>
-                <div className='CreateUserPageButtons'>
-                    {/* this will take the input fields info and test login, if successful goes to main gameMenu to load or start new */}
-                    <button onClick={this.createNewUser}>Create Account</button>
-                    <button onClick={this.toLoginPage}>Login</button>
-                    <button onClick={this.toGameMenu}>Back To Start Menu</button>
+                <div>
+                    <form onSubmit={this.createNewUser}>
+                        <label>
+                            Email:
+                            <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Password:
+                            <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+                        </label>
+                            <input type="submit" value="Create Account" />
+                    </form>
                 </div>
+                {/* <div className='CreateUserPageButtons'> */}
+                    {/* this will take the input fields info and test login, if successful goes to main gameMenu to load or start new */}
+                    {/* <button onClick={this.createNewUser}>Create Account</button> */}
+                    {/* <button onClick={this.toLoginPage}>Login</button> */}
+                    {/* <button onClick={this.toGameMenu}>Back To Start Menu</button> */}
+                {/* </div> */}
             </div>
         )
     }
     
     // this will create the user and add them to the DB
     // then if successfull will log them in and bring to menu
-    createNewUser = () => {
-
+    createNewUser = async (event) => {
+            alert('A name was submitted: ' + this.state);
+            event.preventDefault();
+            const response = await Axios({
+                method: 'get',
+                url: '/check',
+                data: qs.stringify(this.state),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+            console.log(response.data);
+        }
+    
+    // this handles updating the text input of the createUser text inputs
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
     }
 
     // this will send user to the login page
